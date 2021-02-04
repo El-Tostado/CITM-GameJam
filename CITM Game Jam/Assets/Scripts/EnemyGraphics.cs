29646c79b91311e81049b23066ebafbe9b05e6ff
Èric Canela <knela96@gmail.com>
2021-02-04 18:58:48 +0100

    fixed merge

---
+++ CITM Game Jam/Assets/Scripts/EnemyGraphics.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class EnemyGraphics : MonoBehaviour
+{
+    public AIPath aiPath;
+    Animator animator;
+
+    private void Start()
+    {
+        animator = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        if (aiPath.desiredVelocity.x <= 0.01f && aiPath.desiredVelocity.x >= -0.01f)
+            animator.SetBool("walking", false);
+        else
+            animator.SetBool("walking", true);
+
+        if (aiPath.desiredVelocity.x >= 0.0001f)
+            GetComponent<SpriteRenderer>().flipX = true;
+
+        else if (aiPath.desiredVelocity.x <= -0.0001f)
+            GetComponent<SpriteRenderer>().flipX = false;
+
+        if (Vector2.Distance(transform.position, GetComponentInParent<AIDestinationSetter>().target.position) <= aiPath.endReachedDistance)
+        {
+            Debug.Log("HitPlayer");
+        }
+    }
+}
---
+++ CITM Game Jam/Assets/Scripts/UpdatePathfindingGrid.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Pathfinding;
+public class UpdatePathfindingGrid : MonoBehaviour
+{
+    public GameObject[] destroyableObjects;
+    private void Start()
+    {
+       
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown("space"))
+        {
+            Bounds bounds = destroyableObjects[0].gameObject.GetComponent<BoxCollider2D>().bounds;
+            destroyableObjects[0].layer = 0;
+            //destroyableObjects[0].SetActive(false);
+            AstarPath.active.UpdateGraphs(bounds);
+        }
+    }
+}
