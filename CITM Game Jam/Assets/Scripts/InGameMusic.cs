67333c55e254cfd973a1a9d452a251d11521a4f6
Èric Canela <knela96@gmail.com>
2021-02-05 18:56:47 +0100

    Update InGameMusic.cs

--- CITM Game Jam/Assets/Scripts/InGameMusic.cs
+++ CITM Game Jam/Assets/Scripts/InGameMusic.cs
@@ -5,9 +5,20 @@ using UnityEngine.SceneManagement;
 
 public class InGameMusic : MonoBehaviour
 {
+
+    public static InGameMusic Instance = null;
+
     void Awake()
     {
-        if(SceneManager.GetActiveScene().name != "TheEnd" || SceneManager.GetActiveScene().name != "Main Menu")
+
+        if (Instance)
+        {
+            Destroy(gameObject);
+            return;
+        }
+        Instance = this;
+
+        if (SceneManager.GetActiveScene().name != "TheEnd" || SceneManager.GetActiveScene().name != "Main Menu")
             DontDestroyOnLoad(this.gameObject);
     }
 
