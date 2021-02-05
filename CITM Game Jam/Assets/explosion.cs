7bc1ff147a23b839578da9ce4b7a60c73becae11
Èric Canela <knela96@gmail.com>
2021-02-05 01:02:53 +0100

    potions combinations

---
+++ CITM Game Jam/Assets/explosion.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class explosion : MonoBehaviour
+{
+    float timer = 0;
+    public float duration = 1;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        if (timer >= duration)
+            Destroy(gameObject);
+    }
+}
