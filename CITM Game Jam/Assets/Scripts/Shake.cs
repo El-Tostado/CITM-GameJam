dea36bff50ddbe792c0fa7f27b76c5f208ab4de9
Òscar Faura Gràcia <36189245+ofaura@users.noreply.github.com>
2021-02-05 14:48:38 +0100

    Update Shake.cs

--- CITM Game Jam/Assets/Scripts/Shake.cs
+++ CITM Game Jam/Assets/Scripts/Shake.cs
@@ -5,7 +5,11 @@ using UnityEngine;
 public class Shake : MonoBehaviour
 {
     public Animator camAnim;
-    
+
+    private void Start()
+    {
+        camAnim = GameObject.FindGameObjectWithTag("MainCamera").GetComponent<Animator>();
+    }
     public void CamShake()
     {
         camAnim.SetTrigger("shake");
