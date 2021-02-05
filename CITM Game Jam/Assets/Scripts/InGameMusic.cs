6c9ca421493ee4dec019a80cdd196b86c4cb72fe
sherzock <jacobo.galofre@gmail.com>
2021-02-05 18:26:08 +0100

    Music prefab

---
+++ CITM Game Jam/Assets/Scripts/InGameMusic.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class InGameMusic : MonoBehaviour
+{
+    void Awake()
+    {
+        if(SceneManager.GetActiveScene().name == "TheEnd" || SceneManager.GetActiveScene().name == "Main Menu")
+            DontDestroyOnLoad(this.gameObject);
+    }
+}
